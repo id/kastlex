96f8285e1f291fe05f7203fe2f87f999483a43ac
Ivan Dyachkov <ivan.dyachkov@klarna.com>
2016-06-18 15:23:23 +0200

    update deps, fix release

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Kastlex.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Kastlex, []},
-     applications: [:phoenix, :cowboy, :logger, :gettext, :erlzk, :brod]]
+     applications: [:phoenix, :cowboy, :logger, :gettext, :erlzk, :brod, :guardian]]
   end
 
   # Specifies which paths to compile per environment.
@@ -33,7 +33,7 @@ defmodule Kastlex.Mixfile do
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
      {:brod, "~> 2.1"},
-     {:exrm, "~> 0.19.9"},
+     {:exrm, "~> 1.0"},
      {:guardian, "~> 0.12.0"},
      {:erlzk, "~> 0.6.2"}]
   end
